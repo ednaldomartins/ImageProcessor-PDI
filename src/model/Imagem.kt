81656c53d43ea56948d6f1bc7dbe3acb1ffe1ad8
ednaldomartins <martinsednaldo@outlook.com>
2019-07-18 17:45:27 -0300

    Criação das classes Pixel e Imagem.

---
+++ src/model/Imagem.kt
@@ -0,0 +1,7 @@
+package model
+
+class Imagem (
+    var width: Int = 0,
+    var height: Int = 0,
+    var pixels: Array<Array<Pixel>>
+)
---
+++ src/model/Pixel.kt
@@ -0,0 +1,8 @@
+package model
+
+class Pixel (
+    var red: Int = 0,
+    var green: Int = 0,
+    var blue: Int = 0,
+    var alpha: Int = 0
+)
