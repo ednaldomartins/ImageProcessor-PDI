d23b16cd4f453c6e3595080ef9d2b46d570b160d
ednaldomartins <martinsednaldo@outlook.com>
2019-07-23 15:30:05 -0300

    remoção de pixelArray, pois será usado apenas em filtros com extensão.

--- src/model/Filter.kt
+++ src/model/Filter.kt
@@ -3,6 +3,5 @@ package model
 import java.awt.image.BufferedImage
 
 interface Filter {
-    //o metodo ja deve receber o pixelArray pronto, COM ou SEM extensao por zeros.
-    fun filtrate(bufferedImage: BufferedImage, pixelArray: Array<Array<Pixel>>)
+    fun filtrate(bufferedImage: BufferedImage)
 }
