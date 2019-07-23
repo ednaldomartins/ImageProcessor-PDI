0d2f79cd7fbcdfe5175e9bcb85b7f03372c7c975
ednaldomartins <martinsednaldo@outlook.com>
2019-07-23 15:33:10 -0300

    filtro sem extensão. Então não usaremos arrayPixel como auxiliar.

--- src/model/GreenFilter.kt
+++ src/model/GreenFilter.kt
@@ -9,18 +9,8 @@ import java.awt.image.BufferedImage
  * created on july 21, 2019                                                                                         *
  *******************************************************************************************************************/
 class GreenFilter: Filter {
-    override fun filtrate(bufferedImage: BufferedImage, pixelArray: Array<Array<Pixel>>) {
-
+    override fun filtrate(bufferedImage: BufferedImage) {
         val imgWriter = bufferedImage.raster
-
-        /*
-         * TODO Implementar usando pixelArray
-         * ainda sera implementado a parte que usaremos o pixelArray para editar a imagem.
-         * O pixelArray sera usado para gerar uma matriz a partir da imagem do bufferedImage.
-         * O pixelArray servira para fazer calculos de forma generica. Exemplo:
-         * podemos usar a matriz pixelArray para criar uma matriz da imagem completando a borda com zeros ou com
-         * repeticao de numeros para poder calcular os pixels que ficam na borda da verdadeira imagem.
-         */
 
         for (i in 0 until bufferedImage.width) {
             for (j in 0 until bufferedImage.height) {
@@ -28,6 +18,5 @@ class GreenFilter: Filter {
                 imgWriter.setPixel(i, j, v)
             }
         }
-
     }
 }
