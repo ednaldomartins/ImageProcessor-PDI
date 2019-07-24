be100d049fd020be741f43dad881f4863797e2e0
ednaldomartins <martinsednaldo@outlook.com>
2019-07-24 12:52:35 -0300

    Filtro verde em preto e branco.

---
+++ src/model/GreenFilterBlackWhite.kt
@@ -0,0 +1,21 @@
+package model
+
+import java.awt.Color
+import java.awt.image.BufferedImage
+
+/********************************************************************************************************************
+ * @author ednaldo martins                                                                                          *
+ * created on july 23, 2019                                                                                         *
+ *******************************************************************************************************************/
+class GreenFilterBlackWhite: Filter {
+    override fun filtrate(bufferedImage: BufferedImage) {
+        val imgWriter = bufferedImage.raster
+
+        for (i in 0 until bufferedImage.width) {
+            for (j in 0 until bufferedImage.height) {
+                val v = intArrayOf(Color(bufferedImage.getRGB(i, j)).green, Color(bufferedImage.getRGB(i, j)).green, Color(bufferedImage.getRGB(i, j)).green)
+                imgWriter.setPixel(i, j, v)
+            }
+        }
+    }
+}
