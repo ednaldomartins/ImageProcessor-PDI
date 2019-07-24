a21231daf9aad7f27851f474f8d2d1cd5e599b93
ednaldomartins <martinsednaldo@outlook.com>
2019-07-24 18:23:08 -0300

    Segundo teste.

--- src/Main.kt
+++ src/Main.kt
@@ -1,11 +1,17 @@
 import control.ImageProcessorController
-import model.GreenFilter
+import model.RedFilterBlackWhite
 import view.showImage
 
 fun main () {
     val imgController = ImageProcessorController()
+    //local da imagem
     imgController.loadImage("..//ImageProcessor//imagens//lena256color.jpg")
+
     //testando filtro verde
-    imgController.processImage( GreenFilter() )
+    //imgController.processImage( GreenFilter() )
+    //teste de filtro vermelho em preto e branco
+    imgController.processImage( RedFilterBlackWhite() )
+
+    //mostrar resultado da imagem
     showImage(imgController.bufferedImage)
 }
