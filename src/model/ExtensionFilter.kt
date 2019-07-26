df473da913e9bf4fb78e1316d407673914287d79
ednaldomartins <martinsednaldo@outlook.com>
2019-07-26 11:22:56 -0300

    adição da máscara que será usada.

--- src/model/ExtensionFilter.kt
+++ src/model/ExtensionFilter.kt
@@ -3,6 +3,7 @@ package model
 import java.awt.image.BufferedImage
 
 interface ExtensionFilter {
+    val mask: Array<FloatArray>
     //o metodo ja deve receber o pixelArray pronto, com extensao por zeros ou extensão por bordas.
     fun filtrate(bufferedImage: BufferedImage, pixelArray: Array<Array<Pixel?>>)
 }
