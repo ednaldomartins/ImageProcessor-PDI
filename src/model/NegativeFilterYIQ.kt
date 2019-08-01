74dfce26f9b5c0526f893019783b7d08d9517868
ednaldomartins <martinsednaldo@outlook.com>
2019-08-01 10:57:02 -0300

    Filtro negativo no sistema YIQ.

---
+++ src/model/NegativeFilterYIQ.kt
@@ -0,0 +1,38 @@
+package model
+
+import util.Converter
+import util.Matrix.copyImageToMatrix
+import java.awt.image.BufferedImage
+
+class NegativeFilterYIQ: Filter {
+
+    override fun filtrate(bufferedImage: BufferedImage) {
+        //cria como nao nulo e envia para uma classe java
+        val pixelMatrix: Array<Array<Pixel?>> = Array(bufferedImage.width) { arrayOfNulls<Pixel>(bufferedImage.height)}
+        //copia pixels da imagem para matriz sem extensao
+        copyImageToMatrix(bufferedImage, pixelMatrix)
+        //converte de RGB para YIQ para poder fazer o negativo nesse sistema de cores
+        Converter.RGBtoYIQ(pixelMatrix)
+        //aplica o mesmo conceito do RGB no sistema YIQ, onde Y = luma. //OBS.: AQUI O 'RED' = Y
+        for (i in 0 until bufferedImage.width) {
+            for (j in 0 until bufferedImage.height) {
+                pixelMatrix[i][j]!!.red = 255-pixelMatrix[i][j]!!.red
+            }
+        }
+        //tras de volta para o RGB
+        Converter.YIQtoRGB(pixelMatrix)
+        //cria o escritor da imagem
+        val imgWriter = bufferedImage.raster
+        val vPixel = intArrayOf(0,0,0)
+        //apos trazer do YIQ para RGB, devemos verificar se os valores estao fora dos limites permitidos no RGB(255)
+        for (i in 0 until bufferedImage.width) {
+            for (j in 0 until bufferedImage.height) {
+                vPixel[0] = if (pixelMatrix[i][j]!!.red > 255) 255 else if (pixelMatrix[i][j]!!.red < 0) 0 else pixelMatrix[i][j]!!.red.toInt()
+                vPixel[1] = if (pixelMatrix[i][j]!!.green > 255) 255 else if (pixelMatrix[i][j]!!.green < 0) 0 else pixelMatrix[i][j]!!.green.toInt()
+                vPixel[2] = if (pixelMatrix[i][j]!!.blue > 255) 255 else if (pixelMatrix[i][j]!!.blue < 0) 0 else pixelMatrix[i][j]!!.blue.toInt()
+                imgWriter.setPixel(i, j, vPixel)
+            }
+        }
+    }
+
+}
