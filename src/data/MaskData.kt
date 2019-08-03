9d554f8a2d65cad6d6890d8cfb26e8cc80d66bd0
ednaldomartins <martinsednaldo@outlook.com>
2019-08-03 20:44:46 -0300

    Método para carregar uma máscara vazia foi criado para ser usado pelo filtro média e mediana.

--- src/data/MaskData.kt
+++ src/data/MaskData.kt
@@ -12,14 +12,18 @@ import java.io.BufferedReader
 class MaskData {
     val PATH = "..//ImageProcessor//in//mask//"
 
-    fun load(maskName: String): Array<FloatArray>? {
-        var mask: Array<FloatArray> = emptyArray()
+    /**
+     * O metodo deve retornar uma mascara Preenchida com valores encontrados dentro do arquivo escolhido.
+     * @param maskName nome do arquivo de leitura para cirar mascara
+     * @return mascara preenchida
+     */
+    fun loadMask(maskName: String): Array<FloatArray>? {
         try {
             val inputStream: InputStream = File(PATH+maskName).inputStream()
             val bufferedReader: BufferedReader = inputStream.bufferedReader()
             //primeira linha = Largura X Altura
             val vSize = bufferedReader.readLine().toUpperCase().split('X')
-            mask =  Array(vSize[0].toInt()) { FloatArray ( vSize[1].toInt() ) }
+            val mask =  Array(vSize[0].toInt()) { FloatArray ( vSize[1].toInt() ) }
             //linha a linha pegar os valores da matriz
             for (i in 0 until mask.size) {
                 val vLine = bufferedReader.readLine().split(' ')
@@ -27,9 +31,27 @@ class MaskData {
                     mask[i][j] = vLine[j].toFloat()
                 }
             }
+            return mask
         } catch (e: IOException) {
             println("Erro na leitura do arquivo ")
+            return Array(0) {FloatArray(0)}
         }
-        return mask
     }
+
+    /**
+     * O metodo deve retornar apenas uma mascara vazia. Essa mascara pode ser usada para o filtro media e mediana.
+     * @param maskName nome do arquivo de leitura para cirar mascara
+     * @return mascara vazia
+     */
+    fun loadEmptyMask(maskName: String): Array<FloatArray>? = try {
+        val inputStream: InputStream = File(PATH+maskName).inputStream()
+        val bufferedReader: BufferedReader = inputStream.bufferedReader()
+        //primeira linha = Largura X Altura
+        val vSize = bufferedReader.readLine().toUpperCase().split('X')
+        Array(vSize[0].toInt()) { FloatArray ( vSize[1].toInt() ) } //return
+    } catch (e: IOException) {
+        println("Erro na leitura do arquivo ")
+        Array(0) { FloatArray (0) } //return
+    }
+
 }
