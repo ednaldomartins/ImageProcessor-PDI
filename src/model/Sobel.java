2bbf0b19e1c9edce5932843e399bfde614d59f61
ednaldomartins <martinsednaldo@outlook.com>
2019-08-06 12:16:53 -0300

    Refinamento de código.

--- src/model/Sobel.java
+++ src/model/Sobel.java
@@ -6,23 +6,23 @@ import java.awt.image.BufferedImage;
 import java.awt.image.WritableRaster;
 
 public class Sobel implements ExtensionFilter{
-    float[][] mask = new MaskData().loadEmptyMask("mascaraVazia3x3.txt");
+    private float[][] mask = new MaskData().loadEmptyMask("mascaraVazia3x3.txt");
     @Override
     public void filtrate( BufferedImage bufferedImage, Pixel[][] pixelArray)  {
-        WritableRaster imgWrite = bufferedImage.getRaster();
+        int w = bufferedImage.getWidth(), h = bufferedImage.getHeight();
         //cores da borda (usaremos edgeColors para nao alterar o pixelArray durante o processo)
         int[][] edgeColors = new int[pixelArray.length][pixelArray[0].length];
         //gradiene maximo (comecamos com -1 para testar no if a primeira vez sem dar nullPointer)
         int maxGradient = -1;
         //percorrer todos os pixels da imagem
-        for (int i = 1; i < pixelArray.length - 1; i++) {
-            for (int j = 1; j < pixelArray[0].length - 1; j++) {
+        for (int i = 0; i < w; i++) {
+            for (int j = 0; j < h; j++) {
                 //temp: guarda os valores convertidos para nivel de cinza, da regiao aonde as duas mascaras serao aplicadas
                 float[] temp = new float[mask.length*mask[0].length];
                 for (int k = 0, n = 0; k < mask.length; k++) {
                     for (int l = 0; l < mask[0].length; l++, n++) {
                         //https://en.wikipedia.org/wiki/Grayscale, calculando a luminancia
-                        temp[n] = (0.2126f * pixelArray[i-1+k][j-1+l].getRed() + 0.7152f * pixelArray[i-1+k][j-1+l].getGreen() + 0.0722f * pixelArray[i-1+k][j-1+l].getBlue());
+                        temp[n] = (0.2126f * pixelArray[i+k][j+l].getRed() + 0.7152f * pixelArray[i+k][j+l].getGreen() + 0.0722f * pixelArray[i+k][j+l].getBlue());
                     }
                 }
                 //sobel vertical * regiao aplicada da imagem
@@ -38,18 +38,14 @@ public class Sobel implements ExtensionFilter{
             }
         }
 
-        double scale = 255.0 / maxGradient;
-        for (int i = 1; i < pixelArray.length - 1; i++) {
-            for (int j = 1; j < pixelArray[0].length - 1; j++) {
-                int edgeColor = edgeColors[i][j];
-                edgeColor = (int)(edgeColor * scale);
-                edgeColor = 0xff000000 | (edgeColor << 16) | (edgeColor << 8) | edgeColor;
-                int[] imagePixel = new int[3];
-                imagePixel[0] = (edgeColor >> 16) & 0xff;
-                imagePixel[1] = (edgeColor >> 8) & 0xff;
-                imagePixel[2] = (edgeColor) & 0xff;
-
-                imgWrite.setPixel( i-1,j-1, imagePixel);
+        WritableRaster imgWrite = bufferedImage.getRaster();
+        //Divide 255 pelo maior gradiente para encontrar um valor para ponderar todos os outros pixels na escala 0-255.
+        float scale = 255.0f / maxGradient;
+        for (int i = 0; i < w; i++) {
+            for (int j = 0; j < h; j++) {
+                int edgeColor = (int)(edgeColors[i+1][j+1] * scale);
+                int[] imagePixel = {edgeColor, edgeColor, edgeColor};
+                imgWrite.setPixel( i,j, imagePixel);
             }
         }
 
