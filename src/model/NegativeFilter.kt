5a31edf55484431cedb49b6bda75d95b686c2635
ednaldomartins <martinsednaldo@outlook.com>
2019-08-07 15:49:42 -0300

    Correção na cor vermelha.

--- src/model/NegativeFilter.kt
+++ src/model/NegativeFilter.kt
@@ -6,7 +6,7 @@ import java.awt.image.BufferedImage
 /********************************************************************************************************************
  * @author Danillo Medeiros                                                                                         *
  * created on july 30, 2019                                                                                         *
- ******************************************************************************************************************/
+ *******************************************************************************************************************/
 
 //filtro negativo, obtem os valores RBG da imagem carregada e faz a subtraçao por 255 ,gerando assim uma imagem negativa
 class NegativeFilter: Filter {
@@ -15,7 +15,7 @@ class NegativeFilter: Filter {
 
         for (i in 0 until bufferedImage.width) {
             for (j in 0 until bufferedImage.height) {
-                val v = intArrayOf(Color(255-bufferedImage.getRGB(i,j)).red, 255 - Color(bufferedImage.getRGB(i,j)).green, 255 - Color(bufferedImage.getRGB(i,j)).blue, Color(bufferedImage.getRGB(i, j)).alpha)
+                val v = intArrayOf(255 - Color(bufferedImage.getRGB(i,j)).red, 255 - Color(bufferedImage.getRGB(i,j)).green, 255 - Color(bufferedImage.getRGB(i,j)).blue, Color(bufferedImage.getRGB(i, j)).alpha)
                 imgWriter.setPixel(i, j, v)
             }
         }
