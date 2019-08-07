645dc6b1180c96b7e46fd69df8fe7b307065318e
ednaldomartins <martinsednaldo@outlook.com>
2019-08-07 15:59:44 -0300

    Correção no for.

--- src/model/BlackWhiteFilter.kt
+++ src/model/BlackWhiteFilter.kt
@@ -9,8 +9,8 @@ class BlackWhiteFilter: ExtensionFilter {
 
     override fun filtrate(bufferedImage: BufferedImage, pixelArray: Array<Array<Pixel?>>) {
         val imgWriter = bufferedImage.raster
-        for (i in 1 until bufferedImage.width) {
-            for (j in 1 until bufferedImage.height) {
+        for (i in 0 until bufferedImage.width) {
+            for (j in 0 until bufferedImage.height) {
                 val mono = (0.2126f * pixelArray[i][j]!!.red + 0.7152f * pixelArray[i][j]!!.green + 0.0722f * pixelArray[i][j]!!.blue).toInt()
                 val v = intArrayOf(mono, mono, mono, Color(bufferedImage.getRGB(i, j)).alpha)
                 imgWriter.setPixel(i, j, v)
