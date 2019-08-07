2036f35ec7a0321ce5e7d2f52dd90f9fe4aeeb5e
ednaldomartins <martinsednaldo@outlook.com>
2019-08-07 00:06:01 -0300

    não parar execução quando fechar janela.

--- src/view/ImageJFrame.kt
+++ src/view/ImageJFrame.kt
@@ -9,12 +9,11 @@ import javax.swing.JPanel
  * @author ednaldo martins                                                                                          *
  * created on july 20, 2019                                                                                         *
  *******************************************************************************************************************/
+private var frame:JFrame = JFrame()
+
 fun showImage(imagem: BufferedImage) {
-    val frame = JFrame()
-    // ! temporário. quando criar uma tela antes dessa, remover esse comando.
-    frame.defaultCloseOperation = JFrame.EXIT_ON_CLOSE
-    //+28 = barra de titulo
-    frame.setSize(imagem.width, imagem.height+28)
+    //+24 = barra de titulo
+    frame.setSize(imagem.width, imagem.height+24)
     val panel = object : JPanel() {
         override fun paintComponent(grafics: Graphics) {
             grafics.drawImage(imagem, 0, 0, imagem.width, imagem.height, this)
