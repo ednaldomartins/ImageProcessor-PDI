ec27205b4959cff9a11f1aabecd06332b1d212cb
ednaldomartins <martinsednaldo@outlook.com>
2019-08-07 16:10:40 -0300

    Cálculo do tempo de execução do filtro.

--- src/control/ImageProcessorController.kt
+++ src/control/ImageProcessorController.kt
@@ -15,16 +15,27 @@ class ImageProcessorController {
     lateinit var bufferedImage: BufferedImage
 
     fun processImage (filter: Filter) {
+        //tempo inicial
+        val time = System.currentTimeMillis()
+
         filter.filtrate(bufferedImage)
+
+        //tempo de execucao
+        println("\n ${System.currentTimeMillis() - time} milissegundos.\n\n")
     }
 
     fun processImage (filter: ExtensionFilter, convolve: Boolean = false) {
+        //tempo inicial
+        val time = System.currentTimeMillis()
         val pixelMatrix = Extension().zeroExtension(bufferedImage, filter.mask)
         if (convolve) {
             Matrix.convolute(filter.mask)
             filter.filtrate(bufferedImage, pixelMatrix )
         } else
             filter.filtrate(bufferedImage, pixelMatrix)
+
+        //tempo de execucao
+        println("\n ${System.currentTimeMillis() - time} milissegundos.\n\n")
     }
 
     fun loadImage(path: String) {
